8cb5359c20591e7bc5910445344f29f5869d52e4
Your Name <you@example.com>
2025-05-28 13:30:06 +0600

    remove: removal of annual-leave-page screen as the requirement said the buttons will be dynamic

--- lib/presentation/Employee/Leave/Screen/annualleave_page_imports.dart
+++
@@ -1,4 +0,0 @@
-import 'package:fleekhr/presentation/Employee/Leave/Widget/leave_summary_card.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-part 'annualleave_page.dart';
